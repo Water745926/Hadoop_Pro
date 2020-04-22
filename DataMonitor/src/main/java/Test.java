9cd811e748b5da08fe785bca4147de13107e6b6e
llijiayu <lijiayu@reyun.com>
2020-04-22 14:45:43 +0800

    Hadoop HDFS空间清理

---
+++ DataMonitor/src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @description: 程序入口
+ * @author: lijiayu
+ * @create: 2020-04-22 14:23
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+
+}
